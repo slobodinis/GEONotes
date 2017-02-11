a83cdfa398db7acc0a6d8ceded9a2f3260fec534
Ivan3 <slobodinis95@gmail.com>
2017-02-11 17:42:50 +0500

    Пример кастомного view, появляющегося при нажатии на маркер

--- src/main/java/com/apps/geo/notes/MainActivity.java
+++ src/main/java/com/apps/geo/notes/MainActivity.java
@@ -2,12 +2,16 @@ package com.apps.geo.notes;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -26,13 +30,43 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-
-        LatLng pos = new LatLng(55.158926, 61.365527);
-        map.addMarker(new MarkerOptions().position(pos).title("SUSU"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(pos));
-        map.moveCamera(CameraUpdateFactory.zoomTo(14));
-
         map.getUiSettings().setZoomControlsEnabled(true);
         map.getUiSettings().setMapToolbarEnabled(false);
+
+        final LatLng pos1 = new LatLng(55.158926, 61.375527);
+        map.addMarker(new MarkerOptions().position(pos1));
+
+        final LatLng pos2 = new LatLng(55.158926, 61.365527);
+        map.addMarker(new MarkerOptions().position(pos2).title("SUSU"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(pos2));
+        map.moveCamera(CameraUpdateFactory.zoomTo(14));
+
+        map.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                if (marker.getPosition().equals(pos2))
+                    return null;
+                return LayoutInflater.from(MainActivity.this).inflate(R.layout.info_window_layout, null);
+            }
+        });
+        map.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                if (marker.getPosition().equals(pos2))
+                    return;
+                marker.hideInfoWindow();
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "acabac", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
